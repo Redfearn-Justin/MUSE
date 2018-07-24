be043097b86f5b05aca2ba635aa0a98d2241830f
GBaxx <gav.bax@gmail.com>
2018-07-24 13:09:23 -0700

    updated with my description button on top of master

--- assets/javascript/discfun.js
+++ assets/javascript/discfun.js
@@ -12,3 +12,18 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+var coll2 = document.getElementsByClassName("info-desc-button");
+var j;
+
+for (j = 0; j < coll2.length; j++) {
+    coll2[j].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
