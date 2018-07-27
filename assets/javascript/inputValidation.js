4495020b55305bc2d0f12ede31f415dfa3f572b4
Sushichan <collin.chew.7188@gmail.com>
2018-07-26 21:25:42 -0700

    search validation reverted

--- assets/javascript/inputValidation.js
+++ assets/javascript/inputValidation.js
@@ -1,25 +1,38 @@
 //disable submit button by default
-$(".search-input-submit").prop('disabled', true);
+// $(".search-input-submit").prop('disabled', true);
 
 //check input for validity and enable
-$(document).on("keyup", ".myInput", function () {
-    var artistSearchValidCheck = $(".myInput").val().trim();
-    var artistAudioDBIdQuery = queryPrefix + "/searchalbum.php?s=" + artistSearchValidCheck;
-    var inputBar = $(".myInput");
-    $.ajax({
-        url: artistAudioDBIdQuery,
-        method: "GET"
-    })
-        .then(function (response) {
-            if (response.album !== null) {
-                $(".search-input-submit").prop('disabled', false);
-                inputBar.removeClass("noInput");
-                inputBar.attr("placeholder", "Search");
-            }
-            else {
-                $(".search-input-submit").prop('disabled', true);
-                inputBar.addClass("noInput");
-                inputBar.attr("placeholder", "Required!");
-            }
-        })
+// $(document).on("keyup", ".myInput", function () {
+//     var artistSearchValidCheck = $(".myInput").val().trim();
+//     var artistAudioDBIdQuery = queryPrefix + "/searchalbum.php?s=" + artistSearchValidCheck;
+//     var inputBar = $(".myInput");
+//     $.ajax({
+//         url: artistAudioDBIdQuery,
+//         method: "GET"
+//     })
+//         .then(function (response) {
+//             if (response.album !== null) {
+//                 $(".search-input-submit").prop('disabled', false);
+//                 inputBar.removeClass("noInput");
+//                 inputBar.attr("placeholder", "Search");
+//             }
+//             else {
+//                 $(".search-input-submit").prop('disabled', true);
+//                 inputBar.addClass("noInput");
+//                 inputBar.attr("placeholder", "Required!");
+//             }
+//         })
+// });
+
+$(".search-bar").on("click", ".search-button-submit", function (event) {
+    var inputBar = $("#myInput");
+    var inputBarContent = inputBar.val().trim();
+    if (inputBarContent.length < 1) {
+        inputBar.addClass("noInput");
+        inputBar.attr("placeholder", "Required!");
+    }
+    else {
+        inputBar.removeClass("noInput");
+        inputBar.attr("placeholder", "Search");
+    }
 });
